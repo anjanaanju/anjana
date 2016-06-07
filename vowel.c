8a9456d8f4720ac6a3312f6cd5edeff9e6fbcc66
anjanaanju <anjana3696@gmail.com>
2016-06-07 10:12:04 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
 	char n;
 	scanf("%c",&n);
 	if((n=='a')||(n=='A')||(n=='e')||(n=='E')||(n=='i')||(n=='I')||(n=='o')||(n=='O')||(n=='u')||(n=='U'))
 	printf("The character is vowel\n");
 	else
-	printf("The character is not a vowel\n");
+	printf("The character is a consonant \n");
 	return 0;
 }
