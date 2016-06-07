dd448c9fcfcccb50235895a942b99eaea212257f
anjanaanju <anjana3696@gmail.com>
2016-06-07 10:10:55 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	char n;
+	scanf("%c",&n);
+	if((n=='a')||(n=='A')||(n=='e')||(n=='E')||(n=='i')||(n=='I')||(n=='o')||(n=='O')||(n=='u')||(n=='U'))
+	printf("The character is vowel\n");
+	else
+	printf("The character is not a vowel\n");
+	return 0;
+}
